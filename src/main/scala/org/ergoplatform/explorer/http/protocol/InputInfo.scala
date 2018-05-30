685e6e1f90d5fc23183fbb8e3ee5217c8a854178
Aleksei <daron666@yandex.ru>
2018-05-30 14:50:39 +0400

    Fix typo in InputInfo

--- src/main/scala/org/ergoplatform/explorer/http/protocol/InputInfo.scala
+++ src/main/scala/org/ergoplatform/explorer/http/protocol/InputInfo.scala
@@ -12,8 +12,8 @@ object InputInfo {
   def apply(i: Input): InputInfo = InputInfo(from16to58(i.id), from16to58(i.outputId), from16to58(i.signature))
 
   implicit val encoder: Encoder[InputInfo] = (i: InputInfo) => Json.obj(
-    ("id,", Json.fromString(i.id)),
-    ("outputId", Json.fromString(i.outputId)),
-    ("signature", Json.fromString(i.signature))
+    "id" -> Json.fromString(i.id),
+    "outputId" -> Json.fromString(i.outputId),
+    "signature" -> Json.fromString(i.signature)
   )
 }
