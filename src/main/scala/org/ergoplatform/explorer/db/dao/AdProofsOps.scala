ec73b0650b2fe46c77632451da9921c1d9759b2a
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-06-25 17:39:51 +0300

    Fix digest misspelling in AdProofs

--- src/main/scala/org/ergoplatform/explorer/db/dao/AdProofsOps.scala
+++ src/main/scala/org/ergoplatform/explorer/db/dao/AdProofsOps.scala
@@ -13,7 +13,7 @@ object AdProofsOps {
   )
 
   def select(headerId: String): Query0[AdProof] =
-    fr"SELECT header_id, proof_bytes, diget FROM node_ad_proofs WHERE header_id = $headerId".query[AdProof]
+    fr"SELECT header_id, proof_bytes, digest FROM node_ad_proofs WHERE header_id = $headerId".query[AdProof]
 
 
 }
