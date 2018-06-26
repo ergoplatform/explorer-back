3756e653d3d44ce22dc401861d23df235f1958b5
Aleksei Terehin <daron666@yandex.ru>
2018-06-26 16:39:37 +0400

    Fix ambigious column reference

--- src/main/scala/org/ergoplatform/explorer/db/dao/BlockListOps.scala
+++ src/main/scala/org/ergoplatform/explorer/db/dao/BlockListOps.scala
@@ -17,10 +17,18 @@ object BlockListOps {
            sortOrder: String,
            startTs: Long,
            endTs: Long): Query0[RawSearchBlock] = {
+    val sortByValue = sortBy match {
+      case "height" => "h.height"
+      case "timestamp" => "h.timestamp"
+      case "txs_count" => "i.txs_count"
+      case "miner_name" => "i.miner_name"
+      case _ => "h.height"
+    }
+
     (fr"SELECT h.id, h.height, h.timestamp, i.txs_count, i.miner_address, i.miner_name, i.block_size" ++
       fr"FROM node_headers h JOIN blocks_info i ON h.id = i.header_id" ++
       fr"WHERE  ((timestamp >= $startTs) AND (timestamp <= $endTs) AND main_chain = TRUE)" ++
-      Fragment.const("ORDER BY " + sortBy + " " + sortOrder) ++
+      Fragment.const("ORDER BY " + sortByValue + " " + sortOrder) ++
       fr"LIMIT ${limit.toLong} OFFSET ${offset.toLong};").query[RawSearchBlock]
   }
 }
