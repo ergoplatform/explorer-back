553f37d97fc475ff5f6e76cb2e2e3065387d19f7
Aleksei <daron666@yandex.ru>
2018-07-02 16:37:44 +0400

    Fix address name in MinerInfo

--- src/main/scala/org/ergoplatform/explorer/http/protocol/MinerInfo.scala
+++ src/main/scala/org/ergoplatform/explorer/http/protocol/MinerInfo.scala
@@ -1,21 +1,13 @@
 package org.ergoplatform.explorer.http.protocol
 
-import io.circe.{Decoder, Encoder, HCursor, Json}
-import org.ergoplatform.explorer.db.models.Miner
+import io.circe.{Encoder, Json}
 
 case class MinerInfo(addressId: String, name: String)
 
 object MinerInfo {
 
   implicit val encoder: Encoder[MinerInfo] = (minerInfo: MinerInfo) => Json.obj(
-    "adressId" -> Json.fromString(minerInfo.addressId),
+    "address" -> Json.fromString(minerInfo.addressId),
     "name" -> Json.fromString(minerInfo.name)
   )
-
-  implicit val decode: Decoder[MinerInfo] = (c: HCursor) => for {
-    address <- c.downField("addressId").as[String]
-    name <- c.downField("name").as[String]
-  } yield MinerInfo(address, name)
-
-  def fromMiner(m: Miner): MinerInfo = MinerInfo(m.address, m.name)
 }
