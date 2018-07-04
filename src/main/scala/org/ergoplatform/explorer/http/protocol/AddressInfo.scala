cf930e0ad713eee4d582687de19a25609264ec80
Aleksei <daron666@yandex.ru>
2018-07-04 16:15:57 +0400

    Fix current and received balance

--- src/main/scala/org/ergoplatform/explorer/http/protocol/AddressInfo.scala
+++ src/main/scala/org/ergoplatform/explorer/http/protocol/AddressInfo.scala
@@ -1,18 +1,17 @@
 package org.ergoplatform.explorer.http.protocol
 
 import io.circe.{Encoder, Json}
-import org.ergoplatform.explorer.db.models.Output
+import org.ergoplatform.explorer.db.models.SpentOutput
 
 case class AddressInfo(id: String, transactionsCount: Int, totalReceived: Long, currentBalance: Long)
 
 object AddressInfo {
 
-  def apply(addressId: String, os: List[Output]): AddressInfo = {
-    val related = os.filter(_.hash == addressId)
-    val count = related.map(_.txId).distinct.length
-    val receivedSummary = related.map(_.value).sum
-    //TODO: count real unspent amount via db query
-    val currentBalance = receivedSummary
+  def apply(addressId: String, spentOutput: List[SpentOutput]): AddressInfo = {
+    val related = spentOutput.filter(_.output.hash == addressId)
+    val count = related.map(_.output.txId).distinct.length
+    val receivedSummary = related.map(_.output.value).sum
+    val currentBalance = related.filter(_.spentTxId.isEmpty).map(_.output.value).sum
 
     AddressInfo(addressId, count, receivedSummary, currentBalance)
   }
