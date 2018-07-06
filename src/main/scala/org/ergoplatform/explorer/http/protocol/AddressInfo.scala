ceea73bfb94c0f21a6d5706c4e22e05efa878fca
Aleksei <daron666@yandex.ru>
2018-07-06 12:00:48 +0400

    Rework address summary info to get only needful data from db

--- src/main/scala/org/ergoplatform/explorer/http/protocol/AddressInfo.scala
+++ src/main/scala/org/ergoplatform/explorer/http/protocol/AddressInfo.scala
@@ -1,25 +1,19 @@
 package org.ergoplatform.explorer.http.protocol
 
 import io.circe.{Encoder, Json}
-import org.ergoplatform.explorer.db.models.SpentOutput
+import org.ergoplatform.explorer.db.models.AddressSummaryData
 
-case class AddressInfo(id: String, transactionsCount: Int, totalReceived: Long, currentBalance: Long)
+case class AddressInfo(id: String, transactionsCount: Long, totalReceived: Long, currentBalance: Long)
 
 object AddressInfo {
 
-  def apply(addressId: String, spentOutput: List[SpentOutput]): AddressInfo = {
-    val related = spentOutput.filter(_.output.hash == addressId)
-    val count = related.map(_.output.txId).distinct.length
-    val receivedSummary = related.map(_.output.value).sum
-    val currentBalance = related.filter(_.spentTxId.isEmpty).map(_.output.value).sum
-
-    AddressInfo(addressId, count, receivedSummary, currentBalance)
-  }
+  def apply(data: AddressSummaryData): AddressInfo =
+    AddressInfo(data.hash, data.txsCount, data.spent + data.unspent, data.unspent)
 
   implicit val encoder: Encoder[AddressInfo] = (a: AddressInfo) => Json.obj(
     "summary" -> Json.obj("id" -> Json.fromString(a.id)),
     "transactions" -> Json.obj(
-      "total" -> Json.fromInt(a.transactionsCount),
+      "total" -> Json.fromLong(a.transactionsCount),
       "totalReceived" -> Json.fromLong(a.totalReceived),
       "balance" ->  Json.fromLong(a.currentBalance)
     )
