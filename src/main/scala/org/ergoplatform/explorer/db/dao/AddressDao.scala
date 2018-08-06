eb4ae20d816bc8c0cce455a0efd868ec1185c7ed
Aleksei <daron666@yandex.ru>
2018-08-06 09:10:24 +0400

    Trigger db clean/recrawl

--- src/main/scala/org/ergoplatform/explorer/db/dao/AddressDao.scala
+++ src/main/scala/org/ergoplatform/explorer/db/dao/AddressDao.scala
@@ -4,7 +4,5 @@ import doobie.free.connection.ConnectionIO
 import org.ergoplatform.explorer.db.models.AddressSummaryData
 
 class AddressDao {
-
   def getAddressData(hash: String): ConnectionIO[AddressSummaryData] = OutputsOps.addressStats(hash).unique
-
 }
