43907a46c8e2329ec78862b920e154c12d457daf
Aleksei <daron666@yandex.ru>
2018-08-21 08:46:54 +0400

    code formatting

--- src/main/scala/org/ergoplatform/explorer/db/dao/AddressDao.scala
+++ src/main/scala/org/ergoplatform/explorer/db/dao/AddressDao.scala
@@ -6,9 +6,9 @@ import doobie.implicits._
 import org.ergoplatform.explorer.db.models.AddressSummaryData
 
 class AddressDao {
-  def getAddressData(hash: String): ConnectionIO[AddressSummaryData] = OutputsOps.addressStats(hash).option.flatMap{
+  def getAddressData(hash: String): ConnectionIO[AddressSummaryData] = OutputsOps.addressStats(hash).option.flatMap {
     case Some(a) => a.pure[ConnectionIO]
-    case None =>  doobie.free.connection.raiseError(
+    case None => doobie.free.connection.raiseError(
       new NoSuchElementException(s"Cannot find address = $hash")
     )
   }
