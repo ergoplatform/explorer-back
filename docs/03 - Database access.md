ff028b4bb60c8b720927fb72355ea82377c40a68
Aleksei <daron666@yandex.ru>
2018-09-11 17:41:51 +0400

    Add info about db scheme

--- docs/03 - Database access.md
+++ docs/03 - Database access.md
@@ -5,3 +5,5 @@ Which consists number of subpackages
 * `dao` contains all dao and ops classes. Ops class is define operations for related models. Dao class is using this operations to cimbine or just to call them.
 * `models` contains all models that being stored in db
 * `mappings` contains mappings for custom data type. Right now there is only one mapping for storing `io.circe.Json` as `JSONB` in postgres
+
+For db scheme please take a look at `/src/main/resources/db/*.sql` files. Those files are being used for migrations.
--- src/main/resources/db/V1__Node_data.sql
+++ src/main/resources/db/V1__Node_data.sql
@@ -1,3 +1,8 @@
+/*
+   Main table, uses for storing headers from ERGO.
+   Also contain flag `main_chain` that points is this block from main-chain
+   or from side-chain.
+ */
 CREATE TABLE node_headers (
   id VARCHAR(64) NOT NULL PRIMARY KEY,
   parent_id VARCHAR(64) NOT NULL,
@@ -14,7 +19,9 @@ CREATE TABLE node_headers (
   interlinks VARCHAR ARRAY NOT NULL,
   main_chain BOOLEAN NOT NULL
 );
-
+/*
+   Headers indexes. By parent_id, height, ts, main_chain
+ */
 CREATE INDEX "node_headers__parent_id" ON node_headers (parent_id);
 CREATE INDEX "node_headers__height" ON node_headers (height);
 CREATE INDEX "node_headers__ts" ON node_headers (timestamp);
@@ -22,6 +29,10 @@ CREATE INDEX "node_headers__main_chain" ON node_headers (main_chain);
 
 ALTER TABLE node_headers OWNER TO ergo;
 
+
+/*
+    Main table that being used for storing stats.
+ */
 CREATE TABLE blocks_info (
   header_id VARCHAR(64) NOT NULL PRIMARY KEY,
   timestamp BIGINT NOT NULL,
@@ -44,10 +55,16 @@ CREATE TABLE blocks_info (
   total_miners_reward BIGINT NOT NULL,
   total_coins_in_txs BIGINT NOT NULL
 );
+/*
+    Stats table indexes. By height and ts.
+ */
 
 CREATE INDEX "blocks_info__height" ON node_headers (height);
 CREATE INDEX "blocks_info__ts" ON node_headers (timestamp);
 
+/*
+    Table that represents ergo transaction.
+ */
 CREATE TABLE node_transactions (
   id VARCHAR(64) NOT NULL,
   header_id VARCHAR(64) NOT NULL,
@@ -58,10 +75,15 @@ CREATE TABLE node_transactions (
 );
 
 ALTER TABLE node_transactions OWNER TO ergo;
-
+/*
+    Indexes that being used by transactions table.
+ */
 CREATE INDEX "node_transactions__header_id" on node_transactions (header_id);
 CREATE INDEX "node_transactions__timestamp" on node_transactions (timestamp);
-
+/*
+    Table that represents inputs in ergo transactions.
+    Has tx_id field that point to the tx where this input was spent.
+ */
 CREATE TABLE node_inputs (
   box_id VARCHAR(64) NOT NULL,
   tx_id VARCHAR(64) NOT NULL,
@@ -71,9 +93,17 @@ CREATE TABLE node_inputs (
 
 ALTER TABLE node_inputs OWNER TO ergo;
 
+/*
+    Indexes that being used by inputs table.
+ */
 CREATE INDEX "node_inputs__tx_id" on node_inputs (tx_id);
 CREATE INDEX "node_inputs__box_id" on node_inputs (box_id);
 
+
+/*
+    Table that represents outputs in ergo transactions.
+    Has tx_id field that point to the tx where this output was created.
+ */
 CREATE TABLE node_outputs (
   box_id VARCHAR(64) NOT NULL,
   tx_id VARCHAR(64) NOT NULL,
@@ -87,11 +117,17 @@ CREATE TABLE node_outputs (
 
 ALTER TABLE node_outputs OWNER to ergo;
 
+/*
+    Indexes that being used by outputs table.
+ */
 CREATE INDEX "node_outputs__box_id" on node_outputs (box_id);
 CREATE INDEX "node_outputs__tx_id" on node_outputs (tx_id);
 CREATE INDEX "node_outputs__hash" on node_outputs (hash);
 CREATE INDEX "node_outputs__ts" on node_outputs (timestamp);
 
+/*
+    Table for storing ad_proofs of ergo blocks
+ */
 CREATE TABLE node_ad_proofs (
   header_id VARCHAR(64) NOT NULL PRIMARY KEY,
   proof_bytes VARCHAR NOT NULL,
@@ -100,6 +136,10 @@ CREATE TABLE node_ad_proofs (
 
 ALTER TABLE node_ad_proofs OWNER TO ergo;
 
+
+/*
+    Table for storing names for known miners.
+ */
 CREATE TABLE known_miners (
   miner_address VARCHAR NOT NULL PRIMARY KEY,
   miner_name VARCHAR NOT NULL
@@ -107,6 +147,9 @@ CREATE TABLE known_miners (
 
 ALTER TABLE known_miners OWNER TO ergo;
 
+/*
+  Initial data for known_miners table.
+ */
 INSERT INTO known_miners(miner_address, miner_name) VALUES
 ('cd0702774a99fa3041fa8d441ac81217c8f1d715c83bac4f7fcc2be3f0d454c6c908fe', 'catena_serv'),
 ('cd07026146d4305b4ee16e3b2e0ccabaab8d2bb3962ab3603b7b1b8a2aec757b54f7c3', 'ergo-hetzber'),
